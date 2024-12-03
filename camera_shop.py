e9b472b3e9d9345e4a83acce236b497030fd1919
danieleror <deror@uvm.edu>
2024-12-03 16:06:54 -0500

    Added some product data and started implementing product menu.

--- camera_shop.py
+++ camera_shop.py
@@ -6,10 +6,10 @@ import sqlite3
 def main():
     connection = sqlite3.connect('dans_camera_shop.db')
     cursor = connection.cursor()
-
+    print('Welcome to Dan\'s Camera Shop!')
     running = True
     while running:
-        print('Welcome to Dan\'s Camera Shop! What would you like to do?')
+        print('Main menu options:')
         print('1) Sales')
         print('2) Customers')
         print('3) Products')
@@ -26,8 +26,30 @@ def main():
             # TODO: implement customers menu
             print('customers menu')
         elif response == 3:  # products menu
-            # TODO: implement products menu
-            print('products menu')
+            print('Products available:')
+
+            # displays products to user
+            products = pd.read_sql('SELECT * FROM products;', connection).values
+            print('SKU  ITEM                          PRICE')
+            for product in products:
+                print(f'{product[0]:<5}{product[1]:30}{product[2]}')
+
+            # gives user option to add/modify or go back to main menu
+            print('Options:\n'
+                  '1) Add new product\n'
+                  '2) Edit existing product\n'
+                  '3) Exit to main menu')
+            response = inpt.get_numerical_input('Please enter 1, 2, or 3:')
+            if not (1 <= response <= 3):
+                print('ERROR: Response out of range!')
+                response = inpt.get_numerical_input('Please enter 1, 2, or 3:')
+            if response == 1:  # user wants to add new product
+                # TODO: implement addition of new product
+                print('add product here')
+            elif response == 2:  # user wants to modify existing product
+                # TODO: implement modification of existing product
+                print('modify product here')
+            # no else, program automatically returns to main menu
         else:  # program exit
             running = False
 
