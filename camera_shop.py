a7442ece03cdf6d20e0a84265a000619cbd695c3
danieleror <deror@uvm.edu>
2024-12-04 17:30:35 -0500

    Functionality nearly complete. Need to debug and handle some errors.

--- camera_shop.py
+++ camera_shop.py
@@ -23,17 +23,19 @@ def main():
         # sales menu
         if response == 1:
             print('1) View all sales\n'
-                  '2) Modify sale\n'
-                  '3) Delete sale\n'
-                  '4) Exit to main menu')
+                  '2) Create new sale\n'
+                  '3) Modify sale (add products here)\n'
+                  '4) Delete sale\n'
+                  '5) Exit to main menu')
 
             # gets choice from user
-            response = inpt.get_integer_input('Please enter a value 1-4:')
-            while not (1 <= response <= 4):
+            response = inpt.get_integer_input('Please enter a value 1-5:')
+            while not (1 <= response <= 5):
                 print('ERROR: Input out of range!')
-                response = inpt.get_integer_input('Please enter a value 1-4:')
+                response = inpt.get_integer_input('Please enter a value 1-5:')
 
-            if response == 1:  # view all sales
+            # view all sales
+            if response == 1:
                 # retrieves all database info and stores it into variable
                 sales = pd.read_sql('SELECT * FROM (sale_info CROSS JOIN sale_orders USING(sale_id)) '
                                     'CROSS JOIN products USING(product_sku) '
@@ -61,11 +63,215 @@ def main():
                     print(f'    {sale[7]:8<} - {sale[6]:4<}x {sale[8]:<20} @ ${sale[9]}')
 
                 print('-------------------------------------------------')
-            elif response == 2:  # modify sale
 
-                print()
-            elif response == 3:  # delete sale
-                print()
+                # gets sales that may not have any orders associated with it yet
+                all_sales = pd.read_sql('SELECT * FROM sale_info;', connection).values
+                additional_sale_ids = []
+                for sale in all_sales:
+                    if sale[0] not in sale_ids_printed:
+                        additional_sale_ids.append(sale[0])
+
+                if len(additional_sale_ids) > 0:
+                    print('Sale IDs with no associated orders:')
+                    for sale_id in additional_sale_ids:
+                        print(sale_id)
+                    print('-------------------------------------------------')
+            # create new sale - creates new sale_info and user can then add products in the modify sale menu
+            if response == 2:
+                sale_date = inpt.get_date_input('Please enter the order date:')  # gets date for new sale
+
+                customers = pd.read_sql('SELECT * FROM customers;', connection).values
+                customer_id = inpt.get_integer_input('Please enter customer\'s ID to assign sale to:')
+                valid_id = False
+                while not valid_id:  # loops until a valid customer id has been given
+                    for customer in customers:
+                        if customer[0] == customer_id:
+                            valid_id = True
+
+                    if not valid_id:
+                        print('ERROR: No customer found with given ID!')
+                        customer_id = inpt.get_integer_input('Please enter customer\'s ID to assign sale to:')
+
+                sales = pd.read_sql('SELECT sale_id FROM sale_info', connection).values
+                new_sale_id = str(int(sales[len(sales) - 1][0]) + 1)
+
+                cursor.execute('''INSERT INTO sale_info VALUES(''' + new_sale_id + ''', 0.0, 
+                        ''' + sale_date + ''', "payment_pending", ''' + str(customer_id) + ''');''')
+
+            # modify sale
+            elif response == 3:
+                sale_ids_available = pd.read_sql('SELECT sale_id FROM sale_info GROUP BY sale_id;', connection).values
+                sale_to_modify = inpt.get_integer_input('Please enter the ID of the sale you\'d like to modify: ')
+                valid_id = False
+                while not valid_id:  # loops until valid id has been given
+                    for sale_id in sale_ids_available:
+                        if sale_id[0] == sale_to_modify:
+                            valid_id = True
+
+                    if not valid_id:
+                        print('ERROR: no sale exists with provided ID!')
+                        sale_to_modify = inpt.get_integer_input('Please enter the ID of the sale you\'d '
+                                                                'like to modify: ')
+
+                sale_data = pd.read_sql('SELECT * FROM ((sale_info CROSS JOIN sale_orders USING(sale_id))'
+                                        'CROSS JOIN products USING (product_sku)) '
+                                        'CROSS JOIN customers USING (customer_id)'
+                                        'WHERE sale_id=' + str(sale_to_modify) + ';', connection).values
+                if len(sale_data) == 0:  # no associated orders. user must add products before making other changes
+                    # TODO: let user add products to order
+                    print('add products here')
+                elif sale_data[0][3] == 'complete':
+                    # cannot modify sale because it is already complete
+                    print('Unable modify sale as it has already been completed!\n'
+                          'If you\'d like to modify a customer\'s data, or that of a product, please do so through '
+                          'their respective menus.')
+                else:  # able to modify sale because it has not been shipped out yet
+                    still_editing = True
+                    while still_editing:  # loops until user is done modifying sale
+                        # shows user current info
+                        print(f'Current sale info:\n'
+                              f'Total Amount: ${sale_data[0][1]}\n'
+                              f'Date Ordered: {sale_data[0][2]}\n'
+                              f'Status: {sale_data[0][3]}\n'
+                              f'Customer ID: {sale_data[0][4]}\n'
+                              f'Customer Name: {sale_data[0][10]}\n'
+                              f'ORDER CONTENTS:')
+                        for order in sale_data:
+                            print(f'    {order[7]:8<} - {order[6]:4<}x {order[8]:<20} @ ${order[9]}')
+
+                        print('Options:\n'
+                              '1) Change status\n'
+                              '2) Assign different customer\n'
+                              '3) Remove products\n'
+                              '4) Add products'
+                              '5) Done modifying')
+                        response = inpt.get_integer_input('Please enter value 1-5:')
+                        while not (1 <= response <= 5):
+                            print('ERROR: Input out of range!')
+                            response = inpt.get_integer_input('Please enter value 1-5:')
+
+                        # change status
+                        if response == 1:
+                            print('Status options:\n'
+                                  '1) payment_pending\n'
+                                  '2) order_received\n'
+                                  '3) picking\n'
+                                  '4) complete')
+                            response = inpt.get_integer_input('Please enter value 1-4:')
+                            while not (1 <= response <= 4):
+                                print('ERROR: Input out of range!')
+                                response = inpt.get_integer_input('Please enter value 1-4:')
+
+                            if response == 1:
+                                new_status = 'payment_pending'
+                            elif response == 2:
+                                new_status = 'order_received'
+                            elif response == 3:
+                                new_status = 'picking'
+                            else:
+                                new_status = 'complete'
+
+                            # changes status
+                            cursor.execute('''UPDATE sale_info SET status="''' + new_status + '''"
+                                    WHERE sale_id=''' + str(sale_to_modify) + ''';''')
+
+                        # reassign customer
+                        elif response == 2:
+                            customers = pd.read_sql('SELECT * FROM customers;', connection).values
+
+                            new_customer_id = inpt.get_integer_input('Please enter new customer ID:')
+                            valid_id = False
+                            while not valid_id:  # loops until correct customer ID has been found
+                                for customer in customers:
+                                    if customer[0] == new_customer_id:
+                                        valid_id = True
+
+                                if not valid_id:
+                                    print('ERROR: Invalid customer ID entered!')
+                                    new_customer_id = inpt.get_integer_input('Please enter new customer ID:')
+
+                            # reassign customer
+                            cursor.execute('''UPDATE sale_info SET customer_id=''' + str(new_customer_id) + '''
+                                    WHERE sale_id=''' + str(sale_to_modify) + ''';''')
+
+                        # Remove products
+                        elif response == 3:
+                            sku_to_remove = inpt.get_integer_input('Please enter the product SKU you wish to remove:')
+                            valid_sku = False
+                            new_total = ''
+                            while not valid_sku:  # loops until valid sku has been selected
+                                for order in sale_data:
+                                    if order[5] == sku_to_remove:
+                                        new_total = str(float(order[1]) - float(order[7]))  # updates total
+                                        valid_sku = True
+
+                                if not valid_sku:
+                                    print('ERROR: SKU not found in sale!')
+                                    sku_to_remove = inpt.get_integer_input(
+                                        'Please enter the product SKU you wish to remove:')
+
+                            # removes product from orders and updates the new sale total
+                            cursor.execute('''DELETE FROM sale_orders WHERE product_sku=''' + str(sku_to_remove) +
+                                           '''AND sale_id=''' + str(sale_to_modify) + ''';''')
+                            cursor.execute('''UPDATE sale_info SET total_amount=''' + new_total + '''
+                                    WHERE sale_id=''' + str(sale_to_modify) + ''';''')
+
+                        # Add products
+                        elif response == 4:
+                            products_available = pd.read_sql('SELECT * FROM products;', connection).values
+
+                            sku_to_add = inpt.get_integer_input('Please enter the product SKU you\'d like to add:')
+                            valid_sku = False
+                            unit_price = 0.0
+                            while not valid_sku:  # loops until valid sku has been selected
+                                for product in products:
+                                    if product[0] == sku_to_add:
+                                        unit_price = float(product[2])
+                                        valid_sku = True
+
+                                if not valid_sku:
+                                    print('ERROR: SKU not found!')
+                                    sku_to_add = inpt.get_integer_input(
+                                        'Please enter the product SKU you\'d like to add:')
+
+                            quantity = inpt.get_integer_input('Please provide quantity:')
+                            while not (1 <= quantity <= 10):
+                                print('ERROR: Quantity must be between 1-10:')
+                                quantity = inpt.get_integer_input('Please provide quantity:')
+
+                            # adds new order assigned to sale, and updates sale total
+                            cursor.execute('''INSERT INTO sale_orders VALUES(''' + str(sale_to_modify) +
+                                           ''', ''' + str(sku_to_add) + ''', ''' + str(quantity) + ''', '''
+                                           + str(quantity * unit_price) + ''');''')
+                            new_total = str(float(sale_data[0][1]) + (quantity * unit_price))
+                            cursor.execute('''UPDATE sale_info SET total_amount=''' + new_total + '''
+                                                                WHERE sale_id=''' + str(sale_to_modify) + ''';''')
+
+                        # done editing
+                        else:
+                            still_editing = False
+
+            # delete sale
+            elif response == 4:
+                sales = pd.read_sql('SELECT * FROM sale_info;', connection).values
+
+                sale_to_delete = inpt.get_integer_input('Please enter sale ID you wish to delete')
+                valid_id = False
+                while not valid_id:
+                    for sale in sales:
+                        if sale[0] == sale_to_delete:
+                            valid_id = True
+
+                    if not valid_id:
+                        print('ERROR: No sale associated with given ID!')
+                        sale_to_delete = inpt.get_integer_input('Please enter sale ID you wish to delete')
+
+                if inpt.get_yes_no_input('This action cannot be undone, are you sure you want to proceed? (y/n)'):
+                    cursor.execute('''DELETE FROM sale_info WHERE sale_id=''' + str(sale_to_delete) + ''';''')
+                    cursor.execute('''DELETE FROM sale_orders WHERE sale_id=''' + str(sale_to_delete) + ''';''')
+                    print('Sale successfully deleted.')
+                else:
+                    print('Cancelled!')
 
             # no else, program automatically returns to main menu
         # customer menu
